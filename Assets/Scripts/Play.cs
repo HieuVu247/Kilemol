6e8d1c200da5393a57df58bd509332ef88c73c7c
HieuVu247 <minehieu.172@gmail.com>
2024-07-18 08:29:40 +0700

    Update nhỏ:  Xóa các đoạn code rác

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -10,11 +10,6 @@ public class Play : MonoBehaviour
     private Vector2 moveVelocity;
     private bool facingRight = true;
     SpriteRenderer sr;
-    public float autoAttackInterval = 0.5f; // Khoảng thời gian giữa các lần tấn công tự động
-
-    private bool isAutoAttacking = false; // Trạng thái tự động tấn công
-    private float autoAttackTimer = 0f; // Bộ đếm thời gian cho tấn công tự động
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,25 +29,6 @@ public class Play : MonoBehaviour
         {
             Flip();
         }
-
-
-        //Tạm thời tắt vì chức năng chưa hoàn thiện, có thể chuyển sang bên Điều kiển cánh tay đòn
-        //if (Input.GetKeyDown(KeyCode.T))
-        //{
-        //    isAutoAttacking = !isAutoAttacking;
-        //    autoAttackTimer = 0f; // Đặt lại bộ đếm thời gian khi chuyển trạng thái
-        //}
-        // Thực hiện tấn công tự động
-        //if (isAutoAttacking)
-        //{
-        //    autoAttackTimer += Time.deltaTime;
-        //    if (autoAttackTimer >= autoAttackInterval)
-        //    {
-        //        Shoot();
-        //        autoAttackTimer = 0f;
-        //    }
-        //}
-
     }
 
     void FixedUpdate()
@@ -65,8 +41,5 @@ public class Play : MonoBehaviour
         //Nhân vật chỉ xoay hoạt ảnh, không xoay Game Object (Transform Scale)
         facingRight = !facingRight;
         sr.flipX = !sr.flipX;
-        //Vector3 scaler = transform.localScale;
-        //scaler.x *= -1;
-        //transform.localScale = scaler;
     }
 }
