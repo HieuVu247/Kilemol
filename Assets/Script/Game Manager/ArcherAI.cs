53b5e5aabbfd158e31ec5d5199991b6f13616274
Phu67 <tranphu1100222@gmail.com>
2024-08-12 20:45:20 +0700

    lính cung thủ
    có level 2
    bắn ra đc đạn

--- Assets/Script/Game Manager/ArcherAI.cs
+++ Assets/Script/Game Manager/ArcherAI.cs
@@ -8,14 +8,16 @@ public class ArcherAI : MonoBehaviour
     public float speed = 2f;  // Tốc độ di chuyển của kẻ địch
     private SpriteRenderer spriteRenderer; // Tham chiếu tới SpriteRenderer để lật sprite
 
-    
-    
+    // Shoot
+    public GameObject bullet;        // Prefab của đạn
+    public float bulletSpeed = 5f;   // Tốc độ của đạn
+    public float timeBtwFire = 2f;   // Thời gian giữa các lần bắn
+    private float fireCooldown;      // Biến đếm ngược thời gian giữa các lần bắn
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
-
+        fireCooldown = timeBtwFire;  // Đặt thời gian đếm ngược ban đầu
     }
 
     void Update()
@@ -31,15 +33,31 @@ public class ArcherAI : MonoBehaviour
         transform.position += direction * speed * Time.deltaTime;
 
         // Xoay mặt kẻ địch về phía người chơi
-        if (direction.x < 0)
+        spriteRenderer.flipX = direction.x < 0;
+
+        // Kiểm tra nếu đủ thời gian để bắn
+        fireCooldown -= Time.deltaTime;
+        if (fireCooldown <= 0f)
         {
-            spriteRenderer.flipX = true; // Lật sprite theo trục X nếu người chơi bên trái
-        }
-        else
-        {
-            spriteRenderer.flipX = false; // Không lật sprite nếu người chơi bên phải
+            fireCooldown = timeBtwFire;  // Đặt lại thời gian giữa các lần bắn
+            EnemyFireBullet();           // Thực hiện việc bắn đạn
         }
     }
 
-    
+    void EnemyFireBullet()
+    {
+        // Tạo một viên đạn mới tại vị trí của enemy
+        GameObject bulletInstance = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        // Lấy thành phần Rigidbody2D của viên đạn
+        Rigidbody2D rb = bulletInstance.GetComponent<Rigidbody2D>();
+
+        // Tính toán hướng bắn dựa trên vị trí người chơi
+        Vector2 shootDirection = (player.position - transform.position).normalized;
+
+        // Thêm lực để bắn đạn về hướng người chơi
+        rb.AddForce(shootDirection * bulletSpeed, ForceMode2D.Impulse);
+    }
+
+
 }
---
+++ Assets/Script/Game Manager/TLMap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TLMap : MonoBehaviour
+{
+    public Vector2 camPos;
+    public Vector2 dir;
+
+    private void Update()
+    {
+        camPos = Camera.main.transform.position;
+        dir.x = camPos.x - transform.position.x;
+        dir.y = camPos.y - transform.position.y;
+
+        if (dir.x > 29) // sang phải
+        {
+            transform.Translate(Vector2.right * 29 * 2);
+        }
+
+        if (dir.y < -29) // sang trái
+        {
+            transform.Translate(Vector2.right * -29 * 2);
+
+        }
+
+        if(dir.y > 17) //đi lên
+        {
+            transform.Translate(Vector2.up * 17 * 2);
+
+        }
+
+        if (dir.y < -17) // đi xuống
+        {
+            transform.Translate(Vector2.up * -17 * 2);
+
+        }
+    }
+}
